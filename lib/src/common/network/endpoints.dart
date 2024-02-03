90060e340275e3ccfb945cdbc91806e56869f6c5
Konstantin Kostin <newuser9929@gmail.com>
2024-02-03 12:00:59 +0100

    Fix endpoint

--- lib/src/common/network/endpoints.dart
+++ lib/src/common/network/endpoints.dart
@@ -1,4 +1,4 @@
 class Endpoints {
-  static const _host = 'https://rentcheck.newuserk.me.uk';
+  static const _host = 'https://rentcheck.newuserkk.me.uk';
   static const apiV1 = '$_host/api/v1/';
 }
